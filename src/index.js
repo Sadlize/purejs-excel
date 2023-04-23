7c749e346efce4cbead004a69e2d05884661c092
Sadlize <sadlize@mail.ru>
2023-04-23 19:12:45 +0300

    feat: add header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import './styles/normalize.scss';
 import './styles/globals.scss';
+
+// Connect css
+import './styles/header.scss';
+
+function titleInputOnClickHandler() {
+  document
+    .getElementById('title-input')
+    .setSelectionRange(0, this.value.length);
+}
+
+document
+  .getElementById('title-input')
+  .addEventListener('click', titleInputOnClickHandler);
