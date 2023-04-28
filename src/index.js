94dc9e1fb45e2e6035260a94ee2572914e98f1e3
Sadlize <sadlize@mail.ru>
2023-04-28 18:27:29 +0300

    refactor: move selector to a constant

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ import Toolbar from 'components/modules/header/Toolbar';
 import Formula from 'components/modules/header/Formula';
 import Table from 'components/modules/table';
 
-const excelHeader = new LayoutComponent('#app', {
+const app = '#app';
+
+const excelHeader = new LayoutComponent(app, {
   children: [Container, Toolbar, Formula],
 });
 
-const excelTable = new LayoutComponent('#app', {
+const excelTable = new LayoutComponent(app, {
   children: [Table],
 });
 
